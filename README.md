171adf008e82a016bc9b7f1d7f448ca21bb7ce41
Licardo <1014660822@qq.com>
2024-09-18 23:32:50 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# RH-Pilot-Local-Importer
+# Pilot and Heats Import Plugin for RotorHazard
+
 This plugin allows you to import pilots and generate heats from a local file in RotorHazard.
+
+## Features
+
+- Import pilots from a local file
+- Automatically create a race class
+- Generate heats based on the imported data
+- Assign pilots to appropriate heats
+
+## Installation
+
+- ### Manually
+
+1. Clone this repository or download the source code.
+2. Place the `pilot_local_importer` folder in your RotorHazard plugins directory `~/RotorHazard/src/server/plugins`.
+3. Install dependancy `pip install openpyxl`
+3. Restart your RotorHazard server.
+
+- ### Commandline
+
+```bash
+cd ~
+sudo rm -r RotorHazard/src/server/plugins/pilot_local_importer
+wget https://github.com/l1cardo/RH-Pilot-Local-Importer/releases/latest/download/pilot_local_importer.zip
+unzip pilot_local_importer.zip -d pilot_local_importer
+cp -r pilot_local_importer RotorHazard/src/server/plugins/
+rm -r pilot_local_importer
+rm pilot_local_importer.zip
+pip install openpyxl
+sudo systemctl restart rotorhazard.service
+```
+
+## Usage
+
+1. Create an Excel file that **MUST** follow the format below:
+![](sheet.png)
+
+- `Class Name`: Any Class Name you want, can not be the same as existed ones
+
+- `Heat`: Heat name, Pilots in the same heat MUST have the same Heat name
+
+- `Pilot`: Pilot name, Callsign will also use Pilot name
+
+- `Frequency`: RH can not set the Frequency using plugin, this is just for convenient purpose
+
+- `Color`: Support following colors and any Hex color code, `Blue, Orange, Green, Red, Yellow, Purple, Cyan, Grey, Any Hex color code #7f679`
+
+2. Navigate to the "Data Management" panel in the Format page.
+
+3. Choose `Import Pilots and Heats` for Importer, and select your file 
+
+4. Click the "Import" button to start the import process.
+
+## License
+
+[MIT](LICENSE)
---
+++ pilot_local_importer/__init__.py
@@ -0,0 +1,170 @@
+import logging
+import re
+from eventmanager import Evt
+from data_import import DataImporter
+from Database import ProgramMethod
+from io import BytesIO
+from openpyxl import load_workbook
+from collections import defaultdict
+
+logger = logging.getLogger(__name__)
+
+
+def import_pilots(importer_class, rhapi, data, args):
+    if not data:
+        return False
+
+    try:
+        excel_file = BytesIO(data)
+        workbook = load_workbook(filename=excel_file)
+        sheet = workbook.active
+    except Exception as ex:
+        logger.error("Unable to import file: {}".format(str(ex)))
+        return False
+
+    heats = defaultdict(list)
+
+    logger.info("Importing Pilots...")
+    for index, row in enumerate(sheet.iter_rows(values_only=True)):
+        if index == 0:
+            class_name = row[0]
+        elif index != 1:
+            pilot_heat = row[0]
+            pilot_name = row[1]
+            pilot_callsign = row[1]
+            pilot_frequency = row[2]
+            pilot_color = get_pilot_color(row[3])
+            pilot = {
+                "heat": pilot_heat,
+                "name": pilot_name,
+                "callsign": pilot_callsign,
+                "frequency": pilot_frequency,
+                "color": pilot_color,
+            }
+
+            existing_pilot = check_existing_pilot(rhapi, pilot)
+            if not existing_pilot:
+                rhapi.db.pilot_add(name=pilot["name"], callsign = pilot["callsign"], color=pilot["color"])
+                logger.info(f"Pilot added: {pilot}")
+            else:
+                logger.info(f"Pilot alredy exists: {pilot}")
+
+            pilot_id = get_pilot_id(rhapi, pilot)
+            heats[pilot_heat].append(pilot_id)
+
+    rhapi.ui.broadcast_pilots()
+    logger.info("Import Pilots complete")
+
+    # Generate heats
+    import_heats(rhapi, class_name, heats)
+
+    return True
+
+
+def import_heats(rhapi, class_name, heats):
+    existing_class = check_existing_class(rhapi, class_name)
+
+    if not existing_class:
+        race_class = rhapi.db.raceclass_add(name=class_name)
+        logger.info(f"Added Race Class: {class_name}")
+
+        logger.info("Importing Heats...")
+        # Create heats and associate them with the race class and format
+        for heat_name, pilot_ids in heats.items():
+            new_heat = rhapi.db.heat_add(name=heat_name)
+
+            # Associate the heat with the race class and format
+            rhapi.db.heat_alter(new_heat.id, raceclass=race_class.id)
+            # Get all slots for this heat
+            slots = rhapi.db.slots_by_heat(new_heat.id)
+
+            for index, pilot_id in enumerate(pilot_ids):
+                slot_id = slots[index].id
+                rhapi.db.slot_alter(
+                    slot_id, method=ProgramMethod.ASSIGN, pilot=pilot_id
+                )
+        rhapi.ui.message_notify(
+            rhapi.__("Race Class, Pilots and Heats imported successfully")
+        )
+        logger.info("Race Class, Pilots and Heats imported successfully")
+
+        rhapi.ui.broadcast_raceclasses()
+        rhapi.ui.broadcast_heats()
+
+    else:
+        rhapi.ui.message_alert(
+            rhapi.__("Race Class name exists, please change to another name")
+        )
+        logger.warning("Race Class name exists, please change to another name")
+
+
+def check_existing_pilot(rhapi, pilot):
+    existing_pilots = rhapi.db.pilots
+    existing = False
+    for existing_pilot in existing_pilots:
+        if existing_pilot.name == pilot["name"] and existing_pilot.callsign == pilot["callsign"]:
+            existing = True
+    return existing
+
+
+def check_existing_class(rhapi, class_name):
+    existing_classes = rhapi.db.raceclasses
+    existing = False
+    for existing_class in existing_classes:
+        if existing_class.name == class_name:
+            existing = True
+    return existing
+
+
+def get_pilot_id(rhapi, pilot):
+    db_pilots = rhapi.db.pilots
+    for db_pilot in db_pilots:
+        if db_pilot.name == pilot["name"]:
+            return db_pilot.id
+    return None
+
+
+def get_pilot_color(color):
+    pilot_color = "#FFFFFF"
+
+    if is_hex(color):
+        pilot_color = color
+    else:
+
+        if color in ("Blue", "蓝"):
+            pilot_color = "#0022ff"
+        elif color in ("Orange", "橙"):
+            pilot_color = "#ff5500"
+        elif color in ("Green", "绿"):
+            pilot_color = "#00ff22"
+        elif color in ("Red", "红"):
+            pilot_color = "#ff0055"
+        elif color in ("Yellow", "黄"):
+            pilot_color = "#ddff00"
+        elif color in ("Purple", "紫"):
+            pilot_color = "#7700ff"
+        elif color in ("Cyan", "青"):
+            pilot_color = "#00ffdd"
+        elif color in ("Grey", "灰"):
+            pilot_color = "#aaaaaa"
+
+    return pilot_color
+
+
+def is_hex(color):
+    if color.startswith("0x") or color.startswith("#"):
+        color = color[2:]
+    return re.fullmatch(r"[0-9A-Fa-f]+", color) is not None
+
+
+def register_handlers(args):
+    args["register_fn"](
+        DataImporter(
+            "Import Pilots and Heats",
+            import_pilots,
+        )
+    )
+
+
+def initialize(rhapi):
+    rhapi.events.on(Evt.DATA_IMPORT_INITIALIZE, register_handlers)
